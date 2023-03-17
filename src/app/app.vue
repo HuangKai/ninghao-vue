86fe90d13b49a8635fa9a520a012cd81603ac6c5
HuangKai <mail.huangkai@gmail.com>
2023-03-17 18:31:36 +0800

    created：组件的生命周期方法

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,3 +1,20 @@
 <template>
-    <h3>NINGHAO</h3>
+    <h3>{{ name }}</h3>
 </template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            name: 'NINGHAO',
+        };
+    },
+    created() {
+        console.log('App 组件已创建！');
+
+        setTimeout(() => {
+            this.name = '宁皓网 🚀';
+        }, 3000);
+    },
+};
+</script>
