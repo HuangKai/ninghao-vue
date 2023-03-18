881f12c7fd46086d5faeaaf352fc02ce9dc9612b
HuangKai <mail.huangkai@gmail.com>
2023-03-18 22:24:43 +0800

    监听子组件的事件

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,21 +1,28 @@
 <template>
   <h3>{{ name }}</h3>
-  <AppButton text="确定"></AppButton>
-  <AppButton />
-  <app-button></app-button>
-  <app-button />
-  <AppDemoButton></AppDemoButton>
+  <AppButton
+    text="确定"
+    @init="onInitAppButton"
+    @tap="onTapAppButton"
+  ></AppButton>
 </template>
 
 <script>
 import AppButton from '../components/app-button.vue';
-import AppDemoButton from '../components/app-demo-button.vue';
 export default {
   data() {
     return {
       name: '宁皓网',
     };
   },
-  components: { AppButton, AppDemoButton },
+  components: { AppButton },
+  methods: {
+    onInitAppButton() {
+      console.log('on init app button');
+    },
+    onTapAppButton(text) {
+      console.log('on tap app button', text);
+    },
+  },
 };
 </script>
