9ce03715c08a325efa0fd7fb16e8b991961a9681
HuangKai <mail.huangkai@gmail.com>
2023-03-18 23:52:31 +0800

    动态组件（Dynamic Components）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,34 +1,15 @@
 <template>
   <h3>{{ name }}</h3>
-  <AppButton @init="onInitAppButton" @tap="onTapAppButton">
-    提交
-  </AppButton>
-  <AppDemoButton>
-    <template #icon>
-      <span>🔍</span>
-    </template>
-    <template #text>
-      搜索
-    </template>
-  </AppDemoButton>
+  <UserProfile></UserProfile>
 </template>
 <script>
-import AppButton from '../components/app-button.vue';
-import AppDemoButton from '../components/app-demo-button.vue';
+import UserProfile from '../components/user-profile.vue';
 export default {
   data() {
     return {
       name: '宁皓网',
     };
   },
-  components: { AppButton, AppDemoButton },
-  methods: {
-    onInitAppButton() {
-      console.log('on init app button');
-    },
-    onTapAppButton(text) {
-      console.log('on tap app button', text);
-    },
-  },
+  components: { UserProfile },
 };
 </script>
