6a599fc4c202085535d161b35cc3370e967bf209
HuangKai <mail.huangkai@gmail.com>
2023-03-18 20:28:13 +0800

    定义与使用 Vue 组件

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,17 +1,19 @@
 <template>
   <h3>{{ name }}</h3>
+  <AppButton></AppButton>
+  <AppButton />
+  <app-button></app-button>
+  <app-button />
 </template>
 
 <script>
+import AppButton from '../components/app-button.vue';
 export default {
   data() {
     return {
       name: '宁皓网',
     };
   },
+  components: { AppButton },
 };
 </script>
-
-<style>
-@import './styles/app.css';
-</style>
