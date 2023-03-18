d45e039e33a1e43ec424c64bd9d43497d3c81201
HuangKai <mail.huangkai@gmail.com>
2023-03-18 16:23:16 +0800

    绑定 Class

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,70 +1,36 @@
 <template>
-    <h3>{{ name }}</h3>
-    {{ processStatus }}
-    <button v-on:click="resetName">重置</button>
-    <button @click="setName">设置</button>
-    <div v-for="(post, index) in postList" :key="post.id">
-        {{ index + 1 }} {{ post.content }} - <small>{{ post.author }}</small>
+  <h3>{{ name }}</h3>
+  <div class="menu">
+    <div
+      :class="['menu-item', { active: currentItem === index }]"
+      @click="currentItem = index"
+      v-for="(item, index) in menuItems"
+      :key="index"
+    >
+      {{ item }}
     </div>
-    <hr>
-    <div v-if="visible">隐藏的内容！</div>
-    <button @click="visible = !visible">{{ visible ? '隐藏' : '显示' }}</button>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            name: 'NINGHAO',
-            postList: [
-                {
-                    id: 1,
-                    content: '故人西辞黄鹤楼，烟花三月下扬州',
-                    author: '李白'
-                }, {
-                    id: 2,
-                    content: '好雨知时节，当春乃发生',
-                    author: '杜甫'
-                }, {
-                    id: 3,
-                    content: '浔阳江头夜送客，枫叶荻花秋瑟瑟',
-                    author: '白居易'
-                },
-            ],
-            visible: true,
-        };
-    },
-    created() {
-        console.log('App 组件已创建！');
-
-        this.setName();
-
-    },
-    methods: {
-        setName() {
-            setTimeout(() => {
-                this.name = '宁皓网 🚀';
-            }, 3000);
-        },
-        resetName() {
-            this.name = 'NINGHAO';
-        },
-    },
-    computed: {
-        processStatus() {
-            return this.name === 'NINGHAO' ? "初始化..." : "成功设置了数据！";
-        }
-    },
-    watch: {
-        name(newName, oldName) {
-            console.log(`name 发生了变化: ${oldName} -> ${newName}`);
-        }
-    },
+  data() {
+    return {
+      name: '宁皓网',
+      menuItems: ['首页', '热门', '发布'],
+      currentItem: 0,
+    };
+  },
 };
 </script>
 
 <style>
-button {
-    margin: 4px;
+.menu {
+  display: flex;
+  gap: 16px;
+}
+
+.active {
+  color: #6435c9;
 }
 </style>
