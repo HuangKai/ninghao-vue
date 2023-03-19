ed9ca38a8b5b316d38872767dc63f456673fce86
HuangKai <mail.huangkai@gmail.com>
2023-03-19 18:02:32 +0800

    $refs：引用模板

--- src/components/user-register.vue
+++ src/components/user-register.vue
@@ -1,5 +1,14 @@
 <template>
   <div>注册用户</div>
-  <input type="text" />
+  <input type="text" ref="username" />
   <input type="text" />
 </template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$refs.username);
+    this.$refs.username.focus();
+  },
+};
+</script>
