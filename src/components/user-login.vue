9bc773a0f54e627e924090df0408a58fd8ff7327
HuangKai <mail.huangkai@gmail.com>
2023-03-20 23:28:56 +0800

    Vue 组件的生命周期方法

--- src/components/user-login.vue
+++ src/components/user-login.vue
@@ -18,5 +18,45 @@ export default {
   components: {
     AppInput,
   },
+
+  // 组件被创建之前的方法
+  beforeCreate() {
+    console.log('user login before create');
+  },
+
+  // 组件被创建时
+  created() {
+    console.log('user login created');
+  },
+
+  // 组件被挂载之前的方法
+  beforeMount() {
+    console.log('user login before mount');
+  },
+
+  // 组件被挂载时
+  mounted() {
+    console.log('user login mounted');
+  },
+
+  // 组件被更新之前的方法
+  beforeUpdate() {
+    console.log('user login before update');
+  },
+
+  // 组件被更新时
+  updated() {
+    console.log('user login updated');
+  },
+
+  // 组件被卸载之前的方法
+  beforeUnmount() {
+    console.log('user login before unmount');
+  },
+
+  // 组件被卸载时
+  unmounted() {
+    console.log('user login unmounted');
+  },
 };
 </script>
