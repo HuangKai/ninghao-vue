7c0afd83225471e1a8ecebe5f24a2131affb1eb1
HuangKai <mail.huangkai@gmail.com>
2023-03-21 01:18:59 +0800

    用代码切换路由地址（$router.push 与 $router.replace）

--- src/app/components/index.vue
+++ src/app/components/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>你好，欢迎访问宁皓网！</div>
+  <div @click="onClickText">你好，欢迎访问宁皓网！</div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onClickText() {
+      // 添加新的浏览历史，点击浏览器后退按钮退回上一页
+      this.$router.push('/about');
+
+      // 替换当前浏览历史，点击浏览器后退按钮会退到空白页
+      // this.$router.replace('/about');
+    },
+  },
+};
+</script>
