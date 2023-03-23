1c4c3a72c18b0dc3df43e60b246f316d5110bf21
HuangKai <mail.huangkai@gmail.com>
2023-03-24 00:22:04 +0800

    mapState

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,11 +1,21 @@
 <template>
-  <h3>{{ $store.state.name }}</h3>
+  <h3>{{ appName }}</h3>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   data() {
     return {};
+  },
+  computed: {
+    // ...mapState(['name']),
+
+    ...mapState({
+      // 给 name 添加 appName 别名
+      appName: 'name',
+    }),
   },
 };
 </script>
