9d0de704f6f77f4c1e3c595a8b450587b5ff8bd6
HuangKai <mail.huangkai@gmail.com>
2023-04-13 01:01:22 +0800

    创建与使用 axios 实例

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -6,28 +6,63 @@ axios.defaults.baseURL = 'http://localhost:3000';
  * 请求拦截器
  */
 axios.interceptors.request.use(
-    config => {
-        console.log('axios 请求拦截器', config);
+  config => {
+    console.log('axios 请求拦截器', config);
 
-        return config;
-    },
-    error => {
-        return Promise.reject(error);
-    },
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
 );
 
 /**
  * 响应拦截器
  */
 axios.interceptors.response.use(
-    response => {
-        console.log('axios 响应拦截器', response);
+  response => {
+    console.log('axios 响应拦截器', response);
 
-        return response;
-    },
-    error => {
-        return Promise.reject(error);
-    },
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  },
 );
 
-export { axios };
+/**
+ * Axios 实例
+ */
+const apiHttpClient = axios.create({
+  baseURL: 'http://localhost:3000',
+});
+
+/**
+ * apiHttpClient 请求拦截器
+ */
+apiHttpClient.interceptors.request.use(
+  config => {
+    console.log('apiHttpClient 请求拦截器', config);
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+/**
+ * apiHttpClient 响应拦截器
+ */
+apiHttpClient.interceptors.response.use(
+  response => {
+    console.log('apiHttpClient 响应拦截器', response);
+
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+export { axios, apiHttpClient };
