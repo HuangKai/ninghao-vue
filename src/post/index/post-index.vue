623d0d429d1c97c689bda726d969e97660737d32
HuangKai <mail.huangkai@gmail.com>
2023-04-18 13:32:58 +0800

    HTTP 客户端（axios）：请求服务端应用接口

--- src/post/index/post-index.vue
+++ src/post/index/post-index.vue
@@ -1,3 +1,15 @@
 <template>
   <div>Post Index</div>
 </template>
+
+<script>
+import { apiHttpClient } from '@/app/app.service';
+
+export default {
+  async created() {
+    const response = await apiHttpClient.get('/posts');
+
+    console.log(response);
+  },
+};
+</script>
