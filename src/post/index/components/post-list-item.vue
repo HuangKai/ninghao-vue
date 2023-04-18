0031a76d204ed51ede8e886fdf84e23b8b4a1e3e
HuangKai <mail.huangkai@gmail.com>
2023-04-18 14:02:56 +0800

    在 Vue 组件内部请求服务接口获取组件需要的数据

--- src/post/index/components/post-list-item.vue
+++ src/post/index/components/post-list-item.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>Post Item</div>
+  <h3>
+    {{ item.title }} - <small>{{ item.user.name }}</small>
+  </h3>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+  props: {
+    item: Object,
+  },
+});
 </script>
--- src/post/index/components/post-list.vue
+++ src/post/index/components/post-list.vue
@@ -1,12 +1,28 @@
 <template>
-  <PostListItem></PostListItem>
+  <PostListItem
+    v-for="post in posts"
+    :item="post"
+    :key="post.id"
+  ></PostListItem>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import PostListItem from './post-list-item.vue';
+import { apiHttpClient } from '@/app/app.service';
 
 export default defineComponent({
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  // async created: {},
+  async created() {
+    const response = await apiHttpClient.get('/posts');
+    this.posts = response.data;
+  },
   components: {
     PostListItem,
   },
